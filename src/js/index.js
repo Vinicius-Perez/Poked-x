772c761ff26b89a6620b6acc325c2241a3372f98
Vinicius-Perez <115801033+Vinicius-Perez@users.noreply.github.com>
2023-01-27 12:53:33 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+const listaSeleçãoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+
+function soma(numero1, numero2){
+    console.log(numero1+numero2)
+}
+
+listaSeleçãoPokemons.forEach(pokemon => {
+    pokemon.addEventListener('click', () => {
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove('aberto')
+
+        const idPokemonSelecionado = pokemon.attributes.id.value
+
+        const cartaoPokemonParaAbrir = document.getElementById('cartao-' + idPokemonSelecionado)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+
+        const pokemonAtivoNaListagem = document.querySelector('.ativo')
+        pokemonAtivoNaListagem.classList.remove('ativo')
+
+        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+        pokemonSelecionadoNaListagem.classList.add('ativo')
+    })
+})
